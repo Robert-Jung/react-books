27d60fb3c7809efdd4112233419257d3d5318058
Robert-Jung <rjinjung@gmail.com>
2017-08-08 23:08:27 -0700

    Adding books reducer.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Learn React' },
+    { title: 'Learn Redux' },
+    { title: 'You don\'t know JS'}
+  ]
+}
